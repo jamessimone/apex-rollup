38bb7f796564e96995c7366dd3cc42e93a4a5e4a
james simone <jamirus99@gmail.com>
2021-01-16 15:08:03 -0700

    Added app, permission set, tab and polished up LWC for kicking off recalculation rollups. Last thing left to do is zero out the rollup field when this process kicks off.

--- rollup/main/default/lwc/rollupForceRecalculation/rollupForceRecalculation.js
+++ rollup/main/default/lwc/rollupForceRecalculation/rollupForceRecalculation.js
@@ -1,50 +1,66 @@
 import { api, LightningElement, track } from 'lwc';
-import performFullRecalculation from '@salesforce/apex/Rollup.performFullRecalculation';
 import getBatchRollupStatus from '@salesforce/apex/Rollup.getBatchRollupStatus';
+import performFullRecalculation from '@salesforce/apex/Rollup.performFullRecalculation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class RollupForceRecalculation extends LightningElement {
-  @api opFieldOnCalcItem;
-  @api lookupFieldOnCalcItem;
-  @api lookupFieldOnLookupObject;
-  @api rollupFieldOnLookupObject;
-  @api lookupSObjectName;
-  @api calcItemSObjectName;
-  @api operationName;
+  @api rollupData = {
+    opFieldOnCalcItem: '',
+    lookupFieldOnCalcItem: '',
+    lookupFieldOnLookupObject: '',
+    rollupFieldOnLookupObject: '',
+    lookupSObjectName: '',
+    calcItemSObjectName: '',
+    operationName: ''
+  };
 
   @track isRollingUp = false;
   @track rollupStatus;
+  @track error = '';
 
-  waitCounter = 0;
-  resolvedBatchStatuses = ['Completed', 'Failed', 'Aborted'];
+  _resolvedBatchStatuses = ['Completed', 'Failed', 'Aborted'];
 
-  async handleSubmit(event) {
-    event.preventDefault();
-
-    const jobId = await performFullRecalculation({
-      opFieldOnCalcItem,
-      lookupFieldOnCalcItem,
-      lookupFieldOnLookupObject,
-      rollupFieldOnLookupObject,
-      lookupSObjectName,
-      calcItemSObjectName,
-      operationName
-    });
-    await this._getBatchJobStatus(jobId, this.waitCounter);
+  handleChange(event) {
+    this.rollupData[event.target.name] = event.target.value;
   }
 
-  async _getBatchJobStatus(jobId, waitCounter) {
+  async handleSubmit(event) {
+    this.error = '';
+    event.preventDefault();
+
+    try {
+      const jobId = await performFullRecalculation(this.rollupData);
+      await this._getBatchJobStatus(jobId);
+    } catch (e) {
+      const event = new ShowToastEvent({
+        title: 'An error occurred while rolling up',
+        message: e.body.message,
+        variant: 'error'
+      });
+      console.error(e); // in the event you dismiss the toast but still want to see the error
+      this.dispatchEvent(event);
+    }
+  }
+
+  async _getBatchJobStatus(jobId) {
     if (!jobId) {
       return;
     }
     this.isRollingUp = true;
+
     this.rollupStatus = await getBatchRollupStatus({ jobId });
 
     // some arbitrary wait time - for a huge batch job, it could take ages to resolve
-    while (waitCounter < 200 && this.resolvedBatchStatuses.includes(this.rollupStatus) == false) {
-      waitCounter++;
-      setInterval(() => {
-        this._getBatchJobStatus(jobId, waitCounter);
-      }, 5000);
-    }
+    const statusPromise = new Promise(resolve => {
+      let timeoutId;
+      if (this._resolvedBatchStatuses.includes(this.rollupStatus) == false) {
+        timeoutId = setTimeout(() => this._getBatchJobStatus(jobId), 3000);
+      } else {
+        this.isRollingUp = false;
+        clearInterval(timeoutId);
+        resolve();
+      }
+    });
+    await statusPromise;
   }
 }
