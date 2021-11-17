d2407a24fec780ce7b9e7cc249488d2742746481
James Simone <jamirus99@gmail.com>
2021-11-17 08:49:13 -0700

    Re-adding updateGithubCommitStatus.sh script to clear status check requirement

---
+++ scripts/updateGithubCommitStatus.sh
@@ -0,0 +1,17 @@
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action Bot"
+git commit -m "Bumping package version from Github Action"
+git push
+
+# Once the commit has been uploaded to Github, we need to callout
+# to have the commit status set to success - otherwise the PR checks
+# won't show up as having been cleared correctly
+
+latestSha=$(git rev-parse HEAD)
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/statuses/$latestSha \
+  -d '{"state":"success", "description": "Build success!", "context": "scratch-org-test"}'
