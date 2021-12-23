e509f5d637809fb9490056853b4d3610e1d70869
James Simone <jamirus99@gmail.com>
2021-12-23 12:18:26 -0700

    Added base logging string to custom object logger plugin

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,10 @@
 // path delimiter incompatibilities ("/" versus "\\", for instance)
 const path = require('path');
 
-const extraCodeCoveragePaths = [
+const resolvePath = fileName => path.resolve(__dirname + '/' + fileName);
+const resolvePaths = paths => paths.map(fileName => resolvePath(fileName));
+
+const extraCodeCoveragePaths = resolvePaths([
   'extra-tests/classes/RollupTestUtils.cls',
   'extra-tests/classes/RollupTests.cls',
   'extra-tests/classes/RollupEvaluatorTests.cls',
@@ -11,13 +14,22 @@ const extraCodeCoveragePaths = [
   'extra-tests/classes/RollupQueryBuilderTests.cls',
   'extra-tests/classes/RollupRecursionItemTests.cls',
   'extra-tests/classes/RollupParentResetProcessorTests.cls'
-].map(fileName => path.resolve(__dirname + '/' + fileName));
-
-const nebulaLoggerAdapterPath = path.resolve(__dirname + '/plugins/NebulaLogger/classes/RollupNebulaLoggerAdapter.cls');
+]);
+const customLoggerPaths = resolvePaths([
+  'plugins/CustomObjectRollupLogger/classes/RollupCustomObjectLogger.cls',
+  'plugins/CustomObjectRollupLogger/classes/RollupLogBatchPurger.cls',
+  'plugins/CustomObjectRollupLogger/classes/RollupLogControl.cls',
+  'plugins/CustomObjectRollupLogger/classes/RollupLogEventHandler.cls',
+  'plugins/CustomObjectRollupLogger/classes/RollupPurgerSchedulable.cls'
+]);
+const nebulaLoggerAdapterPath = resolvePath('plugins/NebulaLogger/classes/RollupNebulaLoggerAdapter.cls');
+const callbackPath = resolvePath('plugins/RollupCallback/classes/RollupDispatch.cls');
 
 let shouldRunSfdxScanner = false;
 let shouldRunExtraCodeCoveragePackageCreation = false;
 let shouldRunNebulaLoggerPackageCreation = false;
+let shouldRunCustomLoggerPackageCreation = false;
+let shouldRunCallbackPackageCreation = false;
 
 module.exports = {
   '**/lwc/*.js': filenames => `eslint ${filenames.join(' ')} --fix`,
@@ -28,12 +40,17 @@ module.exports = {
       }
 
       const resolvedPath = path.resolve(filename);
-
       if (extraCodeCoveragePaths.includes(resolvedPath)) {
         shouldRunExtraCodeCoveragePackageCreation = true;
       }
       if (nebulaLoggerAdapterPath === resolvedPath) {
         shouldRunNebulaLoggerPackageCreation = true;
+      }
+      if (customLoggerPaths.includes(resolvedPath)) {
+        shouldRunCustomLoggerPackageCreation = true;
+      }
+      if (callbackPath === resolvedPath) {
+        shouldRunCallbackPackageCreation = true;
       }
       return `prettier --write '${filename}'`;
     });
@@ -46,6 +63,12 @@ module.exports = {
     }
     if (shouldRunNebulaLoggerPackageCreation) {
       commands.push('npm run create:package:nebula:adapter');
+    }
+    if (shouldRunCustomLoggerPackageCreation) {
+      commands.push('npm run create:package:logger');
+    }
+    if (shouldRunCallbackPackageCreation) {
+      commands.push('npm run create:package:callback');
     }
     return commands;
   }
