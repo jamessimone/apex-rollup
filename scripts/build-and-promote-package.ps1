61c8cf72074510f29a2d4b106d6132e7843770c0
James Simone <16430727+jamessimone@users.noreply.github.com>
2022-01-11 18:14:38 -0700

    V1.3.23 - BigDecimal bugfixes & Plugin Infrastructure work (#231)
    * Fixing some RollupDateLiteral edge cases that came up with the new year
    * fixes #229 by preventing BigDecimal issue from occurring in all RollupCalculator code paths
    * Replacing Write-Output with Write-Debug and moved error/debug handling to shared Init function thanks to @jongpie
    * Only run RollupParentResetProcessor for full recalcs started from app
    * Standardizing plugin infrastructure within base package, bumping RollupCallback / RollupCustomObjectLogger / Rollup Extra Code Coverage plugin versions
    * Adding script to automate package promotion during merge to main in pipeline
    * WIP - removed promotePackages script, moved function into helper-functions for package promotion, plugin incrementing now checks whether or not current version has been released prior to incrementing. Still need to modify some of this piece before it can be used for rollup base package
    * Cleaned up scripts, fully ported over all package creation to the generic generatePackage.ps1 script
    * Fixes #230 by adding proper COUNT support and default value support for checkbox fields
    * Updating package-lock.json (which was unintentionally behind due to npm install not running correctly when yarn.lock is also present)
    * Bumping package version from Github Action

--- scripts/build-and-promote-package.ps1
+++ scripts/build-and-promote-package.ps1
@@ -3,70 +3,30 @@
 # the versionDescription and versionNumber for the default package. Using those two pieces of information,
 # this script generates a new package version Id per unique Action run, and promotes that package on merges to main
 # it also updates the Ids referenced in the README, and bumps the package version number in the package.json file
+$DebugPreference = 'Continue'
 $ErrorActionPreference = 'Stop'
-. .\scripts\helper-functions.ps1
+. .\scripts\generatePackage.ps1
 
 function Get-Current-Git-Branch() {
   Invoke-Expression 'git rev-parse --abbrev-ref HEAD'
 }
 
-function Get-Apex-Rollup-Package-Alias {
-  param (
-    $packageVersion
-  )
-  $matchString = ".0-0"
-  if ($packageVersion.EndsWith($matchString)) {
-    $packageVersion = $packageVersion.Substring(0, $packageVersion.Length - $matchString.Length);
+function Start-Package-Promotion {
+  Write-Debug "Beginning promote script"
+
+  $allReadmes = Get-ChildItem -Exclude node_modules, .sfdx, tests | Get-ChildItem -Filter README.md -Recurse
+  foreach ($readme in $allReadmes) {
+    $readmePackageIdResults = (Select-String -Path $readme 'https:\/\/login.salesforce.com\/packaging\/installPackage.apexp\?p0=.{0,18}')
+    if ($readmePackageIdResults.Matches.Length -gt 0) {
+      $packageIdSplit = $readmePackageId.Matches[0].Value.Split("=")
+      if ($packageIdSplit.Length -eq 2) {
+        $packageId = $packageIdSplit[1]
+        Write-Debug "Promoting $packageId from $readme"
+        npx sfdx force:package:version:promote -p $packageId -n
+      }
+    }
   }
-  return "apex-rollup@$packageVersion-0"
-}
-
-$loggerClassPath = "./rollup/core/classes/RollupLogger.cls"
-function Get-Package-JSON {
-  Get-Content -Path ./package.json | ConvertFrom-Json
-}
-
-function Get-Logger-Class {
-  Get-Content -Raw -Path $loggerClassPath
-}
-
-function Update-Logger-Class {
-  param (
-      $versionNumber
-  )
-  $versionNumber = "v" + $versionNumber
-  $loggerClassContents = Get-Logger-Class
-  Write-Output "Bumping RollupLogger.cls version number to: $versionNumber"
-
-  $targetRegEx = "(.+CURRENT_VERSION_NUMBER = ')(.+)(';)"
-  $replacementRegEx = '$1' + $versionNumber + '$3'
-  $loggerClassContents -replace $targetRegEx, $replacementRegEx | Set-Content -Path $loggerClassPath -NoNewline
-  npx prettier --write $loggerClassPath
-  git add $loggerClassPath
-}
-
-function Update-Last-Substring {
-  param(
-      [string]$str,
-      [string]$substr,
-      [string]$newstr
-  )
-
-  return $str.Remove(($lastIndex = $str.LastIndexOf($substr)),$substr.Length).Insert($lastIndex,$newstr)
-}
-
-function Get-Latest-Package-Id {
-  param(
-    $currentPackageVersion,
-    $priorPackageVersionNumber
-  )
-  $currentPackageVersionId = $null
-  try {
-    $currentPackageVersionId = (Get-SFDX-Project-JSON).packageAliases | Select-Object -ExpandProperty (Get-Apex-Rollup-Package-Alias $currentPackageVersion)
-  } catch {
-    $currentPackageVersionId = (Get-SFDX-Project-JSON).packageAliases | Select-Object -ExpandProperty (Get-Apex-Rollup-Package-Alias $priorPackageVersionNumber)
-  }
-  return $currentPackageVersionId
+  Write-Debug "Finished package promotion!"
 }
 
 if(Test-Path ".\PACKAGING_SFDX_URL.txt") {
@@ -76,75 +36,10 @@ if(Test-Path ".\PACKAGING_SFDX_URL.txt") {
   throw 'No packaging auth info!'
 }
 
-$sfdxProjectJson = Get-SFDX-Project-JSON
-$currentPackageVersion = $sfdxProjectJson.packageDirectories[0].versionNumber.Remove($sfdxProjectJson.packageDirectories[0].versionNumber.Length - 2, 2)
-
-Write-Output "Current package version number: $currentPackageVersion"
-
-# Cache the prior package version Id to replace in the README
-$priorPackageVersionId = $null
-$priorPackageVersionNumber = $null;
-try {
-  $priorPackageVersionNumber = $currentPackageVersion
-  $priorPackageVersionId = $sfdxProjectJson.packageAliases[0] | Select-Object -ExpandProperty (Get-Apex-Rollup-Package-Alias $priorPackageVersionNumber)
-} catch {
-  # if there hasn't been a current version of the package, get the previous version and its associated package Id
-  $currentPackageNumber = ([int]($currentPackageVersion | Select-String -Pattern '(\d|\d\d)$').Matches.Value)
-  $currentPackageNumberString = $currentPackageNumber.ToString()
-  $priorPackageVersionString = ($currentPackageNumber - 1).ToString()
-
-  $priorPackageVersionNumber = (Update-Last-Substring $currentPackageVersion $currentPackageNumberString $priorPackageVersionString)
-  try {
-    $priorPackageVersionId = $sfdxProjectJson.packageAliases[0] | Select-Object -ExpandProperty (Get-Apex-Rollup-Package-Alias $priorPackageVersionNumber)
-  } catch {
-    $allPackages = $sfdxProjectJson.packageAliases[0] | Select-Object -ExcludeProperty 'Nebula Logger*' | Get-Member -MemberType NoteProperty | Select-Object
-    $priorPackageVersionFull = $allPackages[$allPackages.Length - 1].Name
-    $apexRollupPosition = $priorPackageVersionFull.IndexOf("@")
-    $priorPackageVersionId = $priorPackageVersionFull.Substring($apexRollupPosition + 1).Split('-')[0]
-    $priorPackageVersionNumber = $priorPackageVersionId
-  }
-}
-
-Write-Output "Prior package version: $priorPackageVersionId"
-Write-Output "Prior package version number: $priorPackageVersionNumber"
-
-# Create/promote package version
-
+# Create/promote package version(s)
 $currentBranch = Get-Current-Git-Branch
-if($currentBranch -eq "main") {
-  Write-Output "Promoting package version"
-  $currentPackageVersionId = Get-Latest-Package-Id $currentPackageVersion $priorPackageVersionNumber
-  try {
-    npx sfdx force:package:version:promote -p $currentPackageVersionId -n
-  } catch {
-    # Make the assumption that the only reason "promote" would fail is if an unrelated change (like changing this script)
-    # triggered a build with an already-promoted package version
-  }
+if ($currentBranch -eq "main") {
+  Start-Package-Promotion
 } else {
-  Update-Logger-Class $currentPackageVersion
-
-  # main is a push-protected branch; only create new package versions as part of PRs against main
-  Write-Output "Creating new package version"
-
-  $packageVersionCreateResult = npx sfdx force:package:version:create -d $sfdxProjectJson.packageDirectories[0].path -x -w 30 -c --json | ConvertFrom-Json
-  git add ./sfdx-project.json
-
-  $currentPackageVersionId = $packageVersionCreateResult.result.SubscriberPackageVersionId
-
-  Write-Output "New package version: $currentPackageVersionId"
-
-  if($currentPackageVersionId -ne $priorPackageVersionId) {
-    Update-Package-Install-Links "./README.md" $currentPackageVersionId
-  }
-
-  $packageJson = Get-Package-JSON
-  if ($packageJson.version -ne $currentPackageVersion) {
-    Write-Output "Bumping package.json version to: $currentPackageVersion"
-
-    $packageJson.version = $currentPackageVersion
-    $packagePath = "./package.json"
-    ConvertTo-Json -InputObject $packageJson | Set-Content -Path $packagePath -NoNewline
-
-    git add $packagePath
-  }
+  Generate -packageName "apex-rollup" -readmePath "./README.md"
 }
