f3bde0a83c87ab0e0932c0fa2c099d0f3d352ce5
James Simone <jamirus99@gmail.com>
2022-12-04 12:37:02 -0700

    Generalized scripts to use passed-in parameters, with validation logic in pwsh for what the expected amount should come out to

--- scripts/integration-test-large-data-volume.ps1
+++ scripts/integration-test-large-data-volume.ps1
@@ -1,18 +1,38 @@
 $ErrorActionPreference = 'Stop'
-$pendingStatus = "PENDING"
-$batchScriptPath = "scripts/start-and-return-batch-status.apex"
-$desiredIntegrationSumAmount = 3362031000
 
 . .\scripts\string-utils.ps1
+
+$pendingStatus = "PENDING"
+$batchScriptPath = "scripts/start-and-return-batch-status.apex"
+$desiredIntegrationSumAmount = 0
+$desiredRecordCountPerIteration = 8000
+$desiredIterations = 10
+
+function Get-Expected-Annual-Revenue() {
+  param(
+    $iterationCount,
+    $sumAmount
+  )
+  if ($iterationCount -lt $desiredIterations) {
+    $currentOffset = $desiredRecordCountPerIteration * $iterationCount
+    $startingOffset = $currentOffset
+    for ($currentOffset; $currentOffset -lT ($startingOffset + $desiredRecordCountPerIteration); $currentOffset++) {
+      $sumAmount += $currentOffset
+    }
+    $iterationCount++
+    $sumAmount = Get-Expected-Annual-Revenue $iterationCount $sumAmount
+  }
+  return $sumAmount
+}
 
 function Get-SFDX-Batch-Status() {
   param($scriptPath, $sleepSeconds)
   $sfdxResponse = (npx sfdx force:apex:execute -f $scriptPath --json) | Join-String
+  Write-Host $sfdxResponse
   $responseObject = ConvertFrom-Json -InputObject $sfdxResponse -Depth 2
   if ($responseObject.result.success -eq $false) {
-    throw "An error occurred! $responseObject.result"
+    throw "An error occurred!"
   }
-  Write-Host $responseObject
   $responseLog = $responseObject.result.logs
   $debugMatches = $responseLog.Split('|DEBUG|');
   Write-Host "Incoming debug matches ..."
@@ -36,7 +56,7 @@ function Set-Proper-Script-Variables() {
   $localReplacementString = "INSERT_UTC_TIME_IN_SECONDS_HERE"
   $desiredAnnualRevenueValue = "0"
   if($firstLine -eq "String SCRIPT_VAR_1 = '$localReplacementString';") {
-    $utcMillis = (Get-Date -UFormat %s)
+    $utcMillis = Get-Date -UFormat %s
     Write-Host "Writing current time into script: $utcMillis"
     $localReplacementString = $utcMillis
     $desiredAnnualRevenueValue = $desiredIntegrationSumAmount
@@ -47,13 +67,20 @@ function Set-Proper-Script-Variables() {
 
 function Start-Opportunity-Creation() {
   Write-Host "Populating records if necessary ..."
-  Get-SFDX-Batch-Status "scripts/record-creation-one-parent-many-children.apex" 1
+  $path = "scripts/record-creation-one-parent-many-children.apex"
+  Find-And-Replace-Content -path $path -searchString "SCRIPT_VAR_1" -replacement "sForce"
+  Find-And-Replace-Content -path $path -searchString "SCRIPT_VAR_2" -replacement $desiredRecordCountPerIteration
+  Find-And-Replace-Content -path $path -searchString "SCRIPT_VAR_3" -replacement $desiredIterations
+  Get-SFDX-Batch-Status $path 1
 }
+
+$desiredIntegrationSumAmount = Get-Expected-Annual-Revenue 0 $desiredIntegrationSumAmount
+Write-Output "Expected sum amount ends up as: $desiredIntegrationSumAmount"
 
 Start-Opportunity-Creation
 
 Write-Host "Beginning integration testing for script $batchScriptPath ..."
 Set-Proper-Script-Variables
 Get-SFDX-Batch-Status $batchScriptPath 30
-# reset file
+# reset batch file
 Set-Proper-Script-Variables
