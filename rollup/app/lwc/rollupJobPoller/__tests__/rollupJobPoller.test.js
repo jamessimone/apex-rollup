9c37069954ab8a5f045a34501ba494a79f2b6999
James Simone <16430727+jamessimone@users.noreply.github.com>
2025-05-30 12:33:56 -0400

    v1.7.19 - RollupFlowBulkProcessor bugfix (#692)
    * Fixes issue reported by infocynic via SFXD where rollup metadata that mixes and matches Is Rollup Started From Parent with the RollupFlowBulkProcessor occasionally causes false positives to be thrown for fields on different SObject types

--- rollup/app/lwc/rollupJobPoller/__tests__/rollupJobPoller.test.js
+++ rollup/app/lwc/rollupJobPoller/__tests__/rollupJobPoller.test.js
@@ -1,4 +1,4 @@
-import { createElement } from 'lwc';
+import { createElement } from '@lwc/engine-dom';
 
 import getBatchRollupStatus from '@salesforce/apex/Rollup.getBatchRollupStatus';
 import RollupJobPoller from 'c/rollupJobPoller';
--- rollup/app/lwc/rollupOrderBy/__tests__/rollupOrderBy.test.js
+++ rollup/app/lwc/rollupOrderBy/__tests__/rollupOrderBy.test.js
@@ -1,4 +1,4 @@
-import { createElement } from 'lwc';
+import { createElement } from '@lwc/engine-dom';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 import getNamespaceInfo from '@salesforce/apex/Rollup.getNamespaceInfo';
