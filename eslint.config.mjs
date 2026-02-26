a9a303cedc40d113e9a4a018792c1ad1ed14c97f
James Simone <16430727+jamessimone@users.noreply.github.com>
2026-02-26 13:10:22 -0500

    Updated to version 5.10.0 of code-analyzer and fixed some new scan violations [ci skip]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,4 @@
-export default [{ ignores: ['**/*.html', '**/*.css', '**/*js-meta.xml', '**/*.json'] }];
+export default [
+  { ignores: ['**/*.html', '**/*.css', '**/*js-meta.xml', '**/*.json'] },
+  { files: ['**/__tests__/*.js'], rules: { '@lwc/lwc/no-unexpected-wire-adapter-usages': 'off' } }
+];
