c19bb007420581c89ac9afda8f06d1f014426d74
James Simone <16430727+jamessimone@users.noreply.github.com>
2026-04-07 14:05:38 -0400

    Update validation script

--- scripts/validateScanCommand.js
+++ scripts/validateScanCommand.js
@@ -4,8 +4,15 @@ const violationOutputFilePaths = ['coverage/pmd-violations.json', 'coverage/lint
 
 violationOutputFilePaths.forEach(path => {
   if (fs.existsSync(path)) {
-    const fileContents = Buffer.from(fs.readFileSync(path));
-    const parsedFileContents = JSON.parse(fileContents.toString());
+    let violations = {
+      violationCounts: { total: 0 }
+    };
+    try {
+      const fileContents = Buffer.from(fs.readFileSync(path));
+      violations = JSON.parse(fileContents.toString());
+    } catch (ex) {
+      console.warn('Error reading file contents:', ex);
+    }
     if (parsedFileContents.violationCounts.total > 0) {
       throw Error(JSON.stringify(parsedFileContents, null, 2));
     }
