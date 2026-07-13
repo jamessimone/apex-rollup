a7ecd4153beeea608ff281438292d1ef1032748d
James Simone <16430727+jamessimone@users.noreply.github.com>
2026-07-13 10:11:54 -0400

    Adds better link routing in rollupDeprecatedHeader LWC

--- rollup/app/lwc/rollupDeprecatedHeader/rollupDeprecatedHeader.js
+++ rollup/app/lwc/rollupDeprecatedHeader/rollupDeprecatedHeader.js
@@ -1,5 +1,47 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { gql, graphql } from 'lightning/graphql';
 
 export default class RollupDeprecatedHeader extends LightningElement {
-  linkToCMDTSetupPage = '/lightning/setup/CustomMetadata/home';
+  fallback = '/lightning/setup/CustomMetadata/home';
+  linkToCMDTSetupPage = '/lightning/setup/CustomMetadata/page?address=%2F{0}%3Fsetupid%3DCustomMetadata';
+
+  get query() {
+    return `
+      query {
+        uiapi {
+          query {
+            EntityDefinition (
+              first: 1
+              where: { DeveloperName: { eq: "Rollup" }}
+            ) {
+              edges {
+                node {
+                  DurableId {
+                    value
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+  }
+
+  get resolvedQuery() {
+    return gql(this.query);
+  }
+
+  @wire(graphql, { query: '$resolvedQuery' })
+  gqlQuery({ data, errors }) {
+    if (data) {
+      if (data.uiapi.query.EntityDefinition.edges.length > 0) {
+        this.linkToCMDTSetupPage = this.linkToCMDTSetupPage.replace('{0}', data.uiapi.query.EntityDefinition.edges[0].node.DurableId.value);
+      } else {
+        this.linkToCMDTSetupPage = this.fallback;
+      }
+    } else if (errors) {
+      this.linkToCMDTSetupPage = this.fallback;
+    }
+  }
 }
